d98227b8a373ae0e4e3d85fb41451d9fd579e98f
vra6a <44337519+vra6a@users.noreply.github.com>
2019-07-27 23:55:52 +0200

    2.4.7
    - webshop added (might get better, might not)

---
+++ source/js/webshop.js
@@ -0,0 +1,35 @@
+$(window).on('scroll', function(){
+	if($(window).scrollTop() > 30) {
+		$('nav').addClass('sticky');
+		$('nav li').addClass('sticky2');
+	}
+	else {
+		$('nav').removeClass('sticky');
+		$('a').removeClass('sticky2');
+	}
+})
+
+const navSlide = () => {
+	const lineBurger = document.querySelector('.line-burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+	lineBurger.addEventListener('click', () => {
+
+		nav.classList.toggle('nav-active');
+
+		navLinks.forEach((link, index) => {
+			if(link.style.animation) {
+				link.style.animation = ''
+			}else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 }s`;
+			}
+		});
+
+		lineBurger.classList.toggle('toggle');
+
+	});
+
+}
+
+navSlide();
